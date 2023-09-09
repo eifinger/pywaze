0db5bc174c38dbefaf257e41403da4add435230f
Kevin Stillhammer <kevin.stillhammer@gmail.com>
2023-09-09 18:15:20 +0200

    wrap httpx.TimeoutException (#12)
    This allows to catch it via the generic WRCError or with the new WRCTimeoutError

--- tests/test_route_calculator.py
+++ tests/test_route_calculator.py
@@ -92,3 +92,14 @@ async def test_calc_all_routes_info(
         route_time, route_distance = list(results.values())[0]
         assert route_time == expected_route_time
         assert route_distance == expected_route_distance
+
+
+async def test_calc_route_info_timeout(timeout_mock):
+    """Test calc_route_info with timeout."""
+
+    async with route_calculator.WazeRouteCalculator() as client:
+        with pytest.raises(route_calculator.WRCTimeoutError):
+            await client.calc_route_info(
+                "50.00332659227126,8.262322651915843",
+                "50.08414976707619,8.247836017342934",
+            )
