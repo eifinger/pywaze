49189da99a4c9a0d15814c4ed4010253fa200a0a
Kevin Stillhammer <kevin.stillhammer@gmail.com>
2024-01-21 20:19:12 +0100

    Exchange methods and Response Model (#20)

--- tests/test_route_calculator.py
+++ tests/test_route_calculator.py
@@ -1,10 +1,10 @@
 """Tests for route_calculator module."""
-import httpx
 import pytest
 from pywaze import route_calculator
 from tests.const import (
     GET_ROUTE_RESPONSE_ADDRESSES,
     GET_ROUTE_RESPONSE_COORDS,
+    GET_ALL_ROUTES_RESPONSE,
 )
 
 
@@ -12,24 +12,200 @@ from tests.const import (
     (
         "start",
         "end",
+        "alternatives",
         "get_route_response",
-        "expected_route_time",
-        "expected_route_distance",
+        "expected_route_times",
+        "expected_route_distances",
+        "expected_route_names",
+        "expected_street_names",
     ),
     (
         (
             "50.00332659227126,8.262322651915843",
             "50.08414976707619,8.247836017342934",
+            1,
             GET_ROUTE_RESPONSE_COORDS,
-            18.4,
-            12.715,
+            [18.4],
+            [12.715],
+            ["B455 - Boelckestraße Wiesbaden"],
+            [
+                [
+                    "Kaiserstraße",
+                    "B40 - Kaiserstraße",
+                    "Ernst-Ludwig-Straße",
+                    "Diether-von-Isenburg-Straße",
+                    "Peter-Altmeier-Allee",
+                    "B40 - Peter-Altmeier-Allee",
+                    "Rheinstraße",
+                    "B40 - Rheinstraße",
+                    "Theodor-Heuss-Brücke",
+                    "B40 - Theodor-Heuss-Brücke",
+                    "Theodor-Heuss-Brücke",
+                    "B40 - Theodor-Heuss-Brücke",
+                    "B40",
+                    "Ludwigsrampe",
+                    "B455 - Ludwigsrampe",
+                    "Boelckestraße",
+                    "B455 - Boelckestraße",
+                    "B455",
+                    "Berliner Straße",
+                    "B455 - Berliner Straße",
+                    "B54 - Berliner Straße",
+                    "Frankfurter Straße",
+                    "> Stadtmitte",
+                    "L3037 - Frankfurter Straße",
+                    "K658 - Frankfurter Straße",
+                    "Bierstadter Straße",
+                    "K659 - Bierstadter Straße",
+                    "Paulinenstraße",
+                ]
+            ],
         ),
         (
             "Kaiserstraße 30 55116 Mainz, Germany",
             "Luisenstraße 30 65185 Wiesbaden, Germany",
+            1,
             GET_ROUTE_RESPONSE_ADDRESSES,
-            18.183333333333334,
-            12.644,
+            [18.183333333333334],
+            [12.644],
+            ["B455 - Boelckestraße Wiesbaden"],
+            [
+                [
+                    "Ernst-Ludwig-Straße",
+                    "Diether-von-Isenburg-Straße",
+                    "Peter-Altmeier-Allee",
+                    "B40 - Peter-Altmeier-Allee",
+                    "Rheinstraße",
+                    "B40 - Rheinstraße",
+                    "Theodor-Heuss-Brücke",
+                    "B40 - Theodor-Heuss-Brücke",
+                    "Theodor-Heuss-Brücke",
+                    "B40 - Theodor-Heuss-Brücke",
+                    "B40",
+                    "Ludwigsrampe",
+                    "B455 - Ludwigsrampe",
+                    "Boelckestraße",
+                    "B455 - Boelckestraße",
+                    "B455",
+                    "Berliner Straße",
+                    "B455 - Berliner Straße",
+                    "B54 - Berliner Straße",
+                    "Frankfurter Straße",
+                    "> Stadtmitte",
+                    "L3037 - Frankfurter Straße",
+                    "Rheinstraße",
+                    "L3037 - Rheinstraße",
+                    "Schwalbacher Straße",
+                    "K651 - Schwalbacher Straße",
+                    "Luisenstraße",
+                ]
+            ],
+        ),
+        (
+            "50.00332659227126,8.262322651915843",
+            "50.08414976707619,8.247836017342934",
+            3,
+            GET_ALL_ROUTES_RESPONSE,
+            [20.5, 25.883333333333333, 28.566666666666666],
+            [12.635, 13.066, 12.957],
+            [
+                "B455 - Boelckestraße Wiesbaden",
+                "K650 - Mainzer Straße Wiesbaden",
+                "A643",
+            ],
+            [
+                [
+                    "Kaiserstraße",
+                    "B40 - Kaiserstraße",
+                    "Kaiser-Friedrich-Straße",
+                    "Große Bleiche",
+                    "Peter-Altmeier-Allee",
+                    "B40 - Peter-Altmeier-Allee",
+                    "Rheinstraße",
+                    "B40 - Rheinstraße",
+                    "Theodor-Heuss-Brücke",
+                    "B40 - Theodor-Heuss-Brücke",
+                    "Theodor-Heuss-Brücke",
+                    "B40 - Theodor-Heuss-Brücke",
+                    "B40",
+                    "Ludwigsrampe",
+                    "B455 - Ludwigsrampe",
+                    "Boelckestraße",
+                    "B455 - Boelckestraße",
+                    "B455",
+                    "Berliner Straße",
+                    "B455 - Berliner Straße",
+                    "B54 - Berliner Straße",
+                    "Frankfurter Straße",
+                    "L3037 - Frankfurter Straße",
+                    "K658 - Frankfurter Straße",
+                    "Bierstadter Straße",
+                    "K659 - Bierstadter Straße",
+                    "Paulinenstraße",
+                ],
+                [
+                    "Kaiserstraße",
+                    "B40 - Kaiserstraße",
+                    "Kaiser-Friedrich-Straße",
+                    "Große Bleiche",
+                    "Peter-Altmeier-Allee",
+                    "B40 - Peter-Altmeier-Allee",
+                    "Rheinstraße",
+                    "B40 - Rheinstraße",
+                    "Theodor-Heuss-Brücke",
+                    "B40 - Theodor-Heuss-Brücke",
+                    "Theodor-Heuss-Brücke",
+                    "B40 - Theodor-Heuss-Brücke",
+                    "B40",
+                    "Rampenstraße",
+                    "L3482 - Rampenstraße",
+                    "Wiesbadener Straße",
+                    "L3482 - Wiesbadener Straße",
+                    "Biebricher Straße",
+                    "K648 - Biebricher Straße",
+                    "Rheingaustraße",
+                    "K648 - Rheingaustraße",
+                    "Glarusstraße",
+                    "K649 - Glarusstraße",
+                    "Breslauer Straße",
+                    "K649 - Breslauer Straße",
+                    "Mainzer Straße",
+                    "K650 - Mainzer Straße",
+                    "> A66 / Wiesbaden-Stadtmitte",
+                    "Lessingstraße",
+                    "Friedrich-Ebert-Allee",
+                    "Wilhelmstraße",
+                    "Christian-Zais-Straße",
+                ],
+                [
+                    "Kaiserstraße",
+                    "B40 - Kaiserstraße",
+                    "Boppstraße",
+                    "L424 - Boppstraße",
+                    "Kaiser-Wilhelm-Ring",
+                    "L424 - Kaiser-Wilhelm-Ring",
+                    "Barbarossaring",
+                    "L424 - Barbarossaring",
+                    "Kaiser-Karl-Ring",
+                    "Rheinallee",
+                    "K6 - Rheinallee",
+                    "K17 - Rheinallee",
+                    "A643 > Frankfurt / Wiesbaden",
+                    "A643",
+                    "A643",
+                    "Schiersteiner Straße",
+                    "B262 - Schiersteiner Straße",
+                    "K644 - Schiersteiner Straße",
+                    "Adelheidstraße",
+                    "K644 - Adelheidstraße",
+                    "Karlstraße",
+                    "K644 - Karlstraße",
+                    "Rheinstraße",
+                    "L3037 - Rheinstraße",
+                    "Wilhelmstraße",
+                    "Christian-Zais-Straße",
+                ],
+            ],
         ),
     ),
 )
@@ -37,61 +213,25 @@ from tests.const import (
     "get_route_mock", "wiesbaden_to_coords_mock", "mainz_to_coords_mock"
 )
 async def test_calc_route_info(
-    start: str, end: str, expected_route_time: float, expected_route_distance: float
+    start: str,
+    end: str,
+    alternatives: int,
+    expected_route_times: list[float],
+    expected_route_distances: list[float],
+    expected_route_names: list[str],
+    expected_street_names: list[list[str]],
 ):
     """Test calc_route_info."""
 
     async with route_calculator.WazeRouteCalculator() as client:
-        route_time, route_distance = await client.calc_route_info(start, end)
-        assert route_time == expected_route_time
-        assert route_distance == expected_route_distance
-
-
-@pytest.mark.parametrize(
-    (
-        "start",
-        "end",
-        "get_route_response",
-        "httpx_client",
-        "expected_route_time",
-        "expected_route_distance",
-    ),
-    (
-        (
-            "50.00332659227126,8.262322651915843",
-            "50.08414976707619,8.247836017342934",
-            GET_ROUTE_RESPONSE_COORDS,
-            None,
-            18.4,
-            12.715,
-        ),
-        (
-            "Kaiserstraße 30 55116 Mainz, Germany",
-            "Luisenstraße 30 65185 Wiesbaden, Germany",
-            GET_ROUTE_RESPONSE_ADDRESSES,
-            httpx.AsyncClient(),
-            18.183333333333334,
-            12.644,
-        ),
-    ),
-)
-@pytest.mark.usefixtures(
-    "get_route_mock", "wiesbaden_to_coords_mock", "mainz_to_coords_mock"
-)
-async def test_calc_all_routes_info(
-    start: str,
-    end: str,
-    httpx_client: httpx.AsyncClient | None,
-    expected_route_time: float,
-    expected_route_distance: float,
-):
-    """Test calc_all_routes_info."""
-
-    async with route_calculator.WazeRouteCalculator(client=httpx_client) as client:
-        results = await client.calc_all_routes_info(start, end)
-        route_time, route_distance = list(results.values())[0]
-        assert route_time == expected_route_time
-        assert route_distance == expected_route_distance
+        routes = await client.calc_routes(start, end, alternatives=alternatives)
+        for alternative in range(alternatives):
+            assert routes[alternative].duration == expected_route_times[alternative]
+            assert routes[alternative].distance == expected_route_distances[alternative]
+            assert routes[alternative].name == expected_route_names[alternative]
+            assert (
+                routes[alternative].street_names == expected_street_names[alternative]
+            )
 
 
 async def test_calc_route_info_timeout(timeout_mock):
@@ -99,7 +239,7 @@ async def test_calc_route_info_timeout(timeout_mock):
 
     async with route_calculator.WazeRouteCalculator() as client:
         with pytest.raises(route_calculator.WRCTimeoutError):
-            await client.calc_route_info(
+            await client.calc_routes(
                 "50.00332659227126,8.262322651915843",
                 "50.08414976707619,8.247836017342934",
             )
